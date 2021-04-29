8f13d55ae164ca0b40c762288c258ef46f9ba867
Po Rith <porith@tiger.attlocal.net>
2021-04-29 11:56:26 -0700

    push to web

--- pages/feed.vue
+++ pages/feed.vue
@@ -6,6 +6,7 @@
         Website updates, blog/vlogs, stock purchases, etc. <br><br>
         Trade updates:<br>
         <hr class="my-1">
+        April 29th, 2021. Buy SPY from recent ATH for SAFE 1 Account. <br>
         April 27th, 2021. Was trying to find a good platform that would allow me to make multiple paper trading accounts under one platform. Tried a bunch of different things but none ended up working as well as I wished. So now, currently, I'm hoping to continue messing with TD's Think or Swim app, and go forward with looking how to put together my own little game, via. this website somehow.<br><br>
         Website updates:<br>
         <hr class="my-1">
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-   <div class="container mt-5">
+   <div class="container mt-4">
      <div class="row">
        <div class="col-md-12">
 
@@ -13,12 +13,13 @@
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        
-         Strictly options. No limit on # of trades. Hyper Aggro or bust. <br><br>
+         Stocks, calls options, and puts options. No limit on # of trades. Hyper Aggro or bust. <br><br>
          Buy 100 SPY 400C 5/21 <br>
          Buy 100 SPY 400C 5/21 <br>
          Buy 100 SPY 400C 5/21 <br><br>
-         History <br>
+         Notes: <br>
+         This is the aggressive, free range account, of which I could plan to eventually "master" at any conceivable level. What I can hope to do here is leverage the different capabilities open to me and use them as an advantage for when the market can move as anyone could expect.
+         <br><br>
       </div>
     </div>
   </div>
@@ -30,44 +31,48 @@
     </h2>
     <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-         Strictly options on SPY. 3-5 trades/month. <br><br>
+         Stocks and calls options. 3-5 trades/month. <br><br>
          Buy 100 SPY 400C 5/21 <br>
          Buy 100 SPY 400C 5/21 <br>
          Buy 100 SPY 400C 5/21 <br>
          Buy 100 SPY 400C 5/21 <br>
          Buy 100 SPY 400C 5/21 <br><br>
-         History <br>
+         Notes: <br>
+         This is the control group used as a reference point for other accounts that may be tolerable of more risk. The only instances I would hope to make a trade in this account is during cases of imminent disaster, and world catastrophe.
+         <br><br>
       </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingThree">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        ACCOUNT 3 – MODERATE 1 ($100,000)
+        ACCOUNT 3 – MODERATE 1 ($100,000/$100,000)
       </button>
     </h2>
     <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-         Full stocks only. No limit on # of trades/month.<br><br>
-         Buy 400 shares SPY at $350.00. <br>
-         Buy 400 shares SPY at $350.00. <br><br>
-         Buy | Sell <br><br>
-         History <br>  
+         Full stocks only. 3-5 trades/month.<br><br>
+         Holdings: 0 Shares. Cash: $100,000.00 <br><br>
+         Notes: <br>
+         This is a moderately risk account. While I am not limited to any number of trade throughout the day/week/year, I am still only trading SPY. In this account, I am hoping to take advantage of market momentum, in case by being in the market when its hot, and being out of it when its not.
+         <br><br>
       </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingFour">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-        ACCOUNT 4 – SAFE 1 ($100,000)
+        ACCOUNT 4 – SAFE 1 ($99,626.80/$100,000)
       </button>
     </h2>
     <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-         Full stocks only. Avg. trades: 4-5/year.<br><br>
-         Buy 400 shares SPY at $350.00. <br>
-         Buy 400 shares SPY at $350.00. <br><br>
-         Buy | Sell <br><br>
+         Full stocks only. Hyper conservative. Control group. Avg. trades: 3-5/year, max.<br><br>
+         Buy 238 shares SPY at $418.60, on 042921. <br><br>
+         Holdings: 233 Shares @ $418.60 Cash: $373.20 <br><br>
+         Notes: <br>
+         This is the control group used as a reference point for other accounts that may be tolerable of more risk. The only instances I would hope to make a trade in this account is during cases of imminent disaster, and world catastrophe. In being a long-term buy and hold account, I will hope to see about starting different accounts of the same type throughout unspecified times in the year.
+         <br><br>
       </div>
     </div>
   </div>
